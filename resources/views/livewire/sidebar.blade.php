376074b0bcbbaac7c4428a497ab8494cfcd9358c
PONG13202 <khunnongmai17915@gmail.com>
2024-12-08 17:42:19 +0700

    First Laravel Coding

---
+++ resources/views/livewire/sidebar.blade.php
@@ -0,0 +1,29 @@
+<div class="sidebar">
+    <div class="sidebar-header">
+        <div class="text-center">jApartment 1.0</div>
+    </div>
+    <div class="sidebar-body">
+        <div class="menu">
+            <ul>
+                <li wire:click="changeMenu('dashboard')" @if ($currentMenu == 'dashboard') class="active" @endif>
+                    <i class="fa-solid fa-chart-line me-2"></i>Dashboard
+                </li>
+                <li wire:click="changeMenu('pay')" @if ($currentMenu == 'pay') class="active" @endif>
+                    <i class="fa-solid fa-building me-2"></i>บันทึกค่าใช้จ่าย
+                </li>
+                <li wire:click="changeMenu('room')" @if ($currentMenu == 'room') class="active" @endif>
+                    <i class="fa-solid fa-home me-2"></i>ห้องพัก
+                </li>
+                <li wire:click="changeMenu('customer')" @if ($currentMenu == 'customer') class="active" @endif>
+                    <i class="fa-solid fa-user me-2"></i>ผู้เข้าพัก
+                </li>
+                <li wire:click="changeMenu('user')" @if ($currentMenu == 'user') class="active" @endif>
+                    <i class="fa-solid fa-gear me-2"></i>ผู้ใช้งาน
+                </li>
+                <li wire:click="changeMenu('company/index')" @if ($currentMenu == 'company/index') class="active" @endif>
+                    <i class="fa-solid fa-building me-2"></i>ข้อมูลสถานประกอบการ
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\PayController;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'dashboard']);
+Route::get('/company/index', [CompanyController::class, 'index']);
+Route::get('/room', [RoomController::class, 'index']);
+Route::get('/customer', [CustomerController::class, 'index']);
+Route::get('/pay', [PayController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);

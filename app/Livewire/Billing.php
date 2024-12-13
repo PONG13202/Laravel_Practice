93ecbe08281dc8912ebf890eb437e9f0a4befdea
PONG13202 <khunnongmai17915@gmail.com>
2024-12-13 18:26:55 +0700

    13/12/2024

---
+++ app/Livewire/Billing.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Models\BillingModel;
+use App\Models\CustomerModel;
+use App\Models\RoomModel;
+use App\Models\OrganizationModel;
+use Illuminate\Support\Facades\Log;
+
+class Billing extends Component {
+    public $showModal = false;
+    public $showModalDelete = false;
+    public $rooms = [];
+    public $billings = [];
+    public $id;
+    public $roomId;
+    public $remark;
+    public $createdAt;
+    public $status;
+    public $amountRent;
+    public $amountWater;
+    public $amountElectric;
+    public $amountInternet;
+    public $amountFitness;
+    public $amountWash;
+    public $amountBin;
+    public $amountEtc;
+    public $customerName;
+    public $customerPhone;
+    public $listStatus = [
+        ['status' => 'wait', 'name' => 'รอชำระเงิน'],
+        ['status' => 'paid', 'name' => 'ชำระเงินแล้ว'],
+        ['status' => 'next', 'name' => 'ขอค้างจ่าย'],
+    ];
+    public $sumAmount = 0;
+    public $roomForDelete;
+    public $waterUnit = 0;
+    public $electricUnit = 0;
+    public $waterCostPerUnit = 0;
+    public $electricCostPerUnit = 0;
+    public $roomNameForEdit = '';
+
+    public function mount() {
+        $this->fetchData();
+        $this->createdAt = date('Y-m-d');
+        $this->status = 'wait';
+    }
+
+    public function fetchData() {
+        $customers = CustomerModel::where('status', 'use')->get();
+        $rooms = [];
+
+        $this->billings = BillingModel::orderBy('id', 'desc')->get();
+
+        foreach ($customers as $customer) {
+            $isBilling = false;
+
+            foreach ($this->billings as $billing) {
+                if ($billing->room_id == $customer->room_id) {
+                    $isBilling = true;
+                    break;
+                }
+            }
+
+            if (!$isBilling) {
+                $rooms[] = [
+                    'id' => $customer->room_id,
+                    'name' => $customer->room->name
+                ];
+            }
+        }
+
+        $this->rooms = $rooms;
+
+        if (count($rooms) > 0) {
+            $this->roomId = $rooms[0]['id'];
+            $this->selectedRoom();
+        }
+    }
+
+    public function render() {
+        return view('livewire.billing');
+    }
+
+    public function openModal() {
+        $this->showModal = true;
+    }
+
+    public function closeModal() {
+        $this->showModal = false;
+    }
+
+    public function selectedRoom() {
+        $room = RoomModel::find($this->roomId);
+        $customer = CustomerModel::where('room_id', $this->roomId)->first();
+        $organization = OrganizationModel::first();
+
+        if ($organization->amount_water > 0) {
+            $this->amountWater = $organization->amount_water;
+        } else {
+            $this->waterCostPerUnit = $organization->amount_water_per_unit;
+        }
+
+        if ($organization->amount_electric_per_unit > 0) {
+            $this->electricCostPerUnit = $organization->amount_electric_per_unit;
+        }
+
+        $this->amountInternet = $organization->amount_internet;
+        $this->amountEtc = $organization->amount_etc;
+
+        $this->customerName = $customer->name;
+        $this->customerPhone = $customer->phone;
+        $this->amountRent = $room->price_per_month;
+
+        $this->computeSumAmount();
+    }
+
+    public function computeSumAmount() {
+        if ($this->waterUnit > 0) {
+            $this->amountWater = $this->waterUnit * $this->waterCostPerUnit;
+        }
+
+        if ($this->electricUnit > 0) {
+            $this->amountElectric = $this->electricUnit * $this->electricCostPerUnit;
+        }
+
+        $this->amountWater = $this->amountWater ?? 0;
+        $this->amountElectric = $this->amountElectric ?? 0;
+
+        $this->sumAmount = $this->amountRent + $this->amountWater + $this->amountElectric
+        + $this->amountInternet + $this->amountFitness + $this->amountWash
+        + $this->amountBin + $this->amountEtc;
+    }
+
+    public function save() {
+        $billing = new BillingModel();
+
+        if ($this->id != null) {
+            $billing = BillingModel::find($this->id);
+        }
+
+        $billing->room_id = $this->roomId;
+        $billing->created_at = $this->createdAt;
+        $billing->status = $this->status;
+        $billing->remark = $this->remark ?? '';
+        $billing->amount_rent = $this->amountRent ?? 0;
+        $billing->amount_water = $this->amountWater ?? 0;
+        $billing->amount_electric = $this->amountElectric ?? 0;
+        $billing->amount_internet = $this->amountInternet ?? 0;
+        $billing->amount_fitness = $this->amountFitness ?? 0;
+        $billing->amount_wash = $this->amountWash ?? 0;
+        $billing->amount_bin = $this->amountBin ?? 0;
+        $billing->amount_etc = $this->amountEtc ?? 0;
+        $billing->save();
+
+        $this->fetchData();
+        $this->closeModal();
+
+        $this->id = null;
+        $this->waterUnit = 0;
+        $this->electricUnit = 0;
+        $this->electricCostPerUnit = 0;
+        $this->waterCostPerUnit = 0;
+    }
+
+    public function openModalEdit($id) {
+        $this->showModal = true;
+        $this->billing = BillingModel::find($id);
+        $this->id = $id;
+        $this->roomId = $this->billing->room_id;
+
+        $this->selectedRoom();
+        $this->amountWater = $this->billing->amount_water;
+        $this->amountElectric = $this->billing->amount_electric;
+
+        $this->roomNameForEdit = $this->billing->room->name;
+
+        $organization = OrganizationModel::first();
+        $this->waterUnit = $this->amountWater / $organization->amount_water_per_unit;
+        $this->electricUnit = $this->amountElectric / $organization->amount_electric_per_unit;
+
+        $this->computeSumAmount();
+    }
+
+    public function closeModalEdit() {
+        $this->showModal = false;
+    }
+
+    public function openModalDelete($id, $name) {
+        $this->showModalDelete = true;
+        $this->id = $id;
+        $this->roomForDelete = $name;
+    }
+
+    public function closeModalDelete() {
+        $this->showModalDelete = false;
+    }
+
+    public function deleteBilling() {
+        $billing = BillingModel::find($this->id);
+        $billing->delete();
+
+        $this->fetchData();
+        $this->closeModalDelete();
+    }
+}
--- app/Livewire/Company.php
+++ app/Livewire/Company.php
@@ -1,49 +1,92 @@
 <?php
+namespace App\Livewire;
 
-namespace App\Models;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+use App\Models\OrganizationModel;
+use Illuminate\Support\Facades\Storage;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
+class Company extends Component
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use WithFileUploads;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'level'
-    ];
+    public $name, $address, $phone, $tax_code, $logo;
+    public $amount_water = 0;
+    public $amount_water_per_unit = 0;
+    public $amount_electric_per_unit = 0;
+    public $amount_internet = 0;
+    public $amount_etc = 0;
+    public $logoUrl;
+    public $flashMessage;
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function mount()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        $this->fetchData();
+    }
+
+    public function fetchData()
+    {
+        $organization = OrganizationModel::first();
+        $this->name = $organization->name ?? '';
+        $this->address = $organization->address ?? '';
+        $this->phone = $organization->phone ?? '';
+        $this->tax_code = $organization->tax_code ?? '';
+        $this->amount_water = $organization->amount_water ?? 0;
+        $this->amount_water_per_unit = $organization->amount_water_per_unit ?? 0;
+        $this->amount_electric_per_unit = $organization->amount_electric_per_unit ?? 0;
+        $this->amount_internet = $organization->amount_internet ?? 0;
+        $this->amount_etc = $organization->amount_etc ?? 0;
+
+        if (isset($organization->logo)) {
+            $this->logoUrl = Storage::disk('public')->url($organization->logo);
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.company');
+    }
+
+    public function save() {
+        $logo = '';
+
+        if ($this->logo) {
+            $logo = $this->logo->store('organizations', 'public');
+        }
+
+        if (OrganizationModel::count() == 0) {
+            $organization = new OrganizationModel();
+        } else {
+            $organization = OrganizationModel::first();
+
+            if ($organization->logo) {
+                if ($logo != '') {
+                    $storage = Storage::disk('public');
+
+                    if ($storage->exists($organization->logo)) {
+                        $storage->delete($organization->logo);
+                    }
+                }
+            }
+        }
+
+        $organization->name = $this->name;
+        $organization->address = $this->address;
+        $organization->phone = $this->phone;
+        $organization->tax_code = $this->tax_code;
+
+        if ($logo != '') {
+            $organization->logo = $logo;
+        }
+
+        $organization->amount_water = $this->amount_water;
+        $organization->amount_water_per_unit = $this->amount_water_per_unit;
+        $organization->amount_electric_per_unit = $this->amount_electric_per_unit;
+        $organization->amount_internet = $this->amount_internet;
+        $organization->amount_etc = $this->amount_etc;
+        $organization->save();
+
+        $this->flashMessage = 'บันทึกข้อมูลสำเร็จ';
+        $this->fetchData();
     }
 }
